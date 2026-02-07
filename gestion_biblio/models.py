9955417b1469f0da84078aaee5cddba4adf51220
plena5m <dushimeorianagratiaplena@gmail.com>
2026-02-07 02:15:26 +0200

    bibliotheque_models.py

--- gestion_biblio/models.py
+++ gestion_biblio/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django .contrib.auth.models import User
 
 # Create your models here.
 class Habitant(models.Model):
-    nom=models.CharField(max_length=100)
-    prenom=models.CharField(max_length=100)
+    id=models.BigAutoField(primary_key=True)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     date_naissance=models.DateField()
+    CNI=models.CharField(max_length=20,unique=True)
     adresse=models.CharField (max_length=100)
     ville=models.CharField (max_length=20)
     telephone=models.CharField (max_length=20)
@@ -13,30 +15,38 @@ class Habitant(models.Model):
     actif=models.BooleanField(default=True)
     amende=models.DecimalField(max_digits=8,decimal_places=2,default= 0)
     def __str__(self):
-        return f"{self.nom} {self.prenom}"
+        return f"{self.user.username}"
 
+class Categorie(models.Model):
+    id=models.BigAutoField(primary_key=True)
+    nom=models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.nom}"
 
+    
+    
 class Ouvrage(models.Model):
+    
     ETAT = [
         ('neuf', 'Neuf'),
         ('bon', 'Bon etat'),
         ('abime', 'Abime'),
     ]
+    id=models.BigAutoField(primary_key=True)
     code = models.CharField(max_length=50, unique=True)
     titre = models.CharField(max_length=200)
     auteur = models.CharField(max_length=150)
-    categorie = models.CharField(max_length=100)
-    isbn = models.CharField(max_length=20, blank=True, null=True)
+    isbn = models.CharField(max_length=20, blank=True, null=True,unique=True)
     annee_publication = models.IntegerField()
     nombre_pages = models.IntegerField()
     langue = models.CharField(max_length=50)
-    resume = models.TextField(blank=True, null=True)
     etat = models.CharField(max_length=10, choices=ETAT, default='bon')
     disponible = models.BooleanField(default=True)
     date_ajout = models.DateField(auto_now_add=True)
+    categorie=models.ForeignKey(Categorie,on_delete=models.PROTECT)
 
     def __str__(self):
-        return f"{self.titre} {self.code}"
+        return f"{self.titre} ({self.code})"
     
     
 class Emprunt(models.Model):
@@ -45,7 +55,7 @@ class Emprunt(models.Model):
         ('retourne', 'Retourne'),
         ('retard', 'En retard'),
     ]
-
+    id=models.BigAutoField(primary_key=True)
     emprunteur = models.ForeignKey(Habitant, on_delete=models.PROTECT, related_name='emprunts')
     livre = models.ForeignKey(Ouvrage, on_delete=models.PROTECT, related_name='emprunts')
     date_emprunt = models.DateField(auto_now_add=True)
@@ -56,7 +66,7 @@ class Emprunt(models.Model):
 
 
     def __str__(self):
-        return f"{self.habitant} {self.ouvrage}"
+        return f"{self.emprunteur} {self.livre}"
 
 
     
